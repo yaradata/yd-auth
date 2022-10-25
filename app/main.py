d2bc3c61e85c0dbf020c7d37b6cedbcc8d5da707
Lumiere Israel <lumiereisrael7@gmail.com>
2022-10-25 18:46:07 +0100

    dockerfile updated

--- app/main.py
+++ app/main.py
@@ -1,9 +1,14 @@
+import uvicorn, os
 from fastapi import FastAPI
 from fastapi.responses import RedirectResponse
 
 
 from databases.database import init_database
 from services import auth
+
+APP_PORT = os.getenv('APP_PORT')
+APP_RELOAD = os.getenv('APP_RELOAD')
+APP_WORKERS = os.getenv('APP_WORKERS')
 
 app = FastAPI()
 
@@ -14,3 +19,6 @@ async def home():
 
 init_database(app)
 app.include_router(auth.auth_router, tags = ['auth'])
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0", port=int(APP_PORT), reload=APP_RELOAD, workers=int(APP_WORKERS)) 
